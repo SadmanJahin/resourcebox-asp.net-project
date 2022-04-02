62191219b6d27df2b9e1c86bc0cf4b4be93cf7fc
Sadman Jahin <sadmanjahin115@gmail.com>
2022-04-02 16:40:28 +0600

    first commit

---
+++ Areas/FAQ/Controllers/FAQController.cs
@@ -0,0 +1,28 @@
+﻿using ResourceBox.Models;
+using ResourceBox.Services.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace ResourceBox.Areas.FAQ.Controllers
+{
+    [RouteArea("FAQ")]
+    public class FAQController : Controller
+    {
+        // GET: FAQ/FAQ
+        public ActionResult FAQ()
+
+        {
+            SecurityDAO db = new SecurityDAO();
+            ViewModel viewModel = new ViewModel();
+            viewModel.Users = db.fetchUser();
+            viewModel.UserChats = db.fetchMessages();
+            viewModel.UserNotifications = db.fetchNotification(Convert.ToInt32(Session["userId"]));
+            return View(viewModel);
+        }
+    }
+}
---
+++ Areas/Features/Controllers/FeaturesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ResourceBox.Models;
+using ResourceBox.Services.Data;
+
+namespace ResourceBox.Areas.Features.Controllers
+{
+    public class FeaturesController : Controller
+    {
+        // GET: Features/Features
+        public ActionResult Features()
+        {
+            SecurityDAO db = new SecurityDAO();
+            ViewBag.MemberCount = db.UserCount();
+            ViewBag.PostCount = db.postCount();
+            ViewModel viewModel = new ViewModel();
+            viewModel.Users = db.fetchUser();
+            viewModel.UserChats = db.fetchMessages();
+            viewModel.UserNotifications = db.fetchNotification(Convert.ToInt32(Session["userId"]));
+            return View(viewModel);
+
+           
+        }
+    }
+}
